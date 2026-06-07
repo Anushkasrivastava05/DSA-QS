400d5b17d1674b2067e02a6b57a0a7283849f687
Anushkasrivastava05 <anushkasrivastavawork5@gmail.com>
2026-06-07 11:59:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (54.15%) - LeetSync

--- 742-to-lower-case/to-lower-case.cpp
+++ 742-to-lower-case/to-lower-case.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        string a =s;
+        
         for(int i =0;i<s.size();i++){
             if(s[i]>=65 && s[i]<=90){
-                 a[i]=a[i]+32;
+                 s[i]=s[i]+32;
             }
         }
-        return a;
+        return s;
     }
 };
